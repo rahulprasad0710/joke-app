a41a966e08ced13f49c50562b0ef7b68844d7bd2
rahulprasad0710 <raulshah2021@hotmail.com>
2022-11-23 01:21:36 +0545

    import all assests and navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Navbar = () => {
+    return (
+        <div className='navbar'>
+            <div className='navbar-container container'>
+                <div className='nav-item'>
+                    <div className='nav-item-text'>SO&nbsp;FUNKTIONIERT'S</div>
+                </div>
+                <div className='nav-item'>
+                    <div className='nav-item-text'>SONDERANGEBOTE</div>
+                </div>
+                <div className='nav-item'>
+                    <div className='nav-item-text'>MEIN BEREICH</div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Navbar;
