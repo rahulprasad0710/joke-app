60335fac12c2cb340ba2e65e45ddf58655e734ef
rahulprasad0710 <raulshah2021@hotmail.com>
2022-11-23 13:43:57 +0545

    added joke item

--- src/components/JokeContainer.jsx
+++ src/components/JokeContainer.jsx
@@ -9,8 +9,9 @@ const JokeContainer = () => {
     const { item, err } = useItem(`${API.getJokes}+${query}`);
 
     return (
-        <div className='container'>
+        <div className='joke-container container'>
             {item &&
+                item.result &&
                 item.result.length > 0 &&
                 item.result.map((jokeItem) => (
                     <JokeItem key={jokeItem.id} jokeItem={jokeItem} />
--- src/components/JokeItem.jsx
+++ src/components/JokeItem.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 
 const JokeItem = ({ jokeItem }) => {
-    return <div>{jokeItem.value}</div>;
+    return (
+        <div className='joke-item'>
+            <div className='joke-title'>
+                {`${jokeItem?.categories[0]}`}&nbsp;JOKE
+            </div>
+            <p className='joke-value'>{jokeItem.value}</p>
+        </div>
+    );
 };
 
 export default JokeItem;
