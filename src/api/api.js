3d6fac4c5a1cc196727d47eefd723c75a0a96a28
rahulprasad0710 <raulshah2021@hotmail.com>
2022-11-24 17:17:12 +0545

    joke mian page css done

--- src/api/api.js
+++ src/api/api.js
@@ -3,6 +3,7 @@ const API_URL = {
     getJokes: "https://api.chucknorris.io/jokes/search?query=",
     getJokeItem: "https://api.chucknorris.io/jokes/",
     getRandomJoke: "https://api.chucknorris.io/jokes/search?query=",
+    getRandomJokeByCatg: "https://api.chucknorris.io/jokes/random?category=",
 };
 
 export default API_URL;
--- src/pages/Joke.jsx
+++ src/pages/Joke.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import { useState, useEffect } from "react";
 import API from "../api/api";
 import { useParams } from "react-router-dom";
 import { useItem } from "../hooks/useItem";
@@ -11,6 +11,15 @@ import likeDown from "../assets/assets_Homework_Front-End_02/hand-copy.png";
 const Joke = () => {
     const { id } = useParams();
     const { item, err } = useItem(`${API.getJokeItem}${id}`);
+    const { item: catg, err: error } = useItem(API.getCategory);
+    const [joke, setJoke] = useState({});
+
+    useEffect(() => {
+        if (item) {
+            setJoke(item);
+        }
+    }, [item]);
+
     return (
         <div className='joke-page'>
             <div className='container'>
@@ -19,10 +28,20 @@ const Joke = () => {
                         <img src={backArrow} alt='Back' />
                     </div>
                 </div>
-                <div>
+                <div className='main-content'>
                     {item && (
                         <div className='joke-box'>
                             <div className='joke-content-container'>
+                                {item &&
+                                item.categories &&
+                                item.categories.length > 0 ? (
+                                    <h2 className='joke-title'>
+                                        {item.categories[0]} JOKE
+                                    </h2>
+                                ) : (
+                                    <h2 className='joke-title'>The Joke</h2>
+                                )}
+
                                 <p>{item.value}</p>
                             </div>
 
@@ -36,16 +55,29 @@ const Joke = () => {
                             </div>
                         </div>
                     )}
-                    <div className='joke-pagination-container'>
-                        <button className='btn-secondary'>
-                            <img src={arrowLeft} alt='Arrow Left' />
-                            PREV JOKES
-                        </button>
-                        <button className='btn-secondary'>
-                            NEXT JOKES
-                            <img src={arrowRight} alt='Arrow Right' />
-                        </button>
-                    </div>
+
+                    {catg && (
+                        <div className='catg-container'>
+                            <h3 className='catg-title'>CATEGORIES</h3>
+                            <ul>
+                                {catg.map((catgItem) => (
+                                    <li>
+                                        <span>{catgItem}</span>
+                                    </li>
+                                ))}
+                            </ul>
+                        </div>
+                    )}
+                </div>
+                <div className='joke-pagination-container'>
+                    <button className='btn-secondary'>
+                        <img src={arrowLeft} alt='Arrow Left' />
+                        PREV JOKES
+                    </button>
+                    <button className='btn-secondary'>
+                        NEXT JOKES
+                        <img src={arrowRight} alt='Arrow Right' />
+                    </button>
                 </div>
             </div>
         </div>
